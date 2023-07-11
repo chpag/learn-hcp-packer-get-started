2b9a6d14f3ef48e2642f03bd1a2d85c42e4f7979
chpag <115650339+chpag@users.noreply.github.com>
2023-07-11 17:25:02 +0200

    Update main.tf

--- tf-channel/main.tf
+++ tf-channel/main.tf
@@ -24,9 +24,23 @@ resource "aws_instance" "app_server" {
   }
 }
 
-check "ami_version_check" {
+check "latest_ami" {
+  # Workaround for check{} blocks currently evaluating against the future
+  # state of the resource: use current state instead, from a data source
+  data "aws_instance" "app_server1" {
+    # Can't use instance_id either, because in the case of a newer AMI, that ID is going to change too
+    # instance_id = aws_instance.web.id
+
+    # So instead... just give me all the EC2 instances in this VPC
+    # That's okay for this use-case, but not generalisable
+    filter {
+      name   = "tag:Name"
+      values = ["Learn-HCP-Packer"]
+    }
+  }
+
   assert {
-    condition = aws_instance.app_server.ami == data.hcp_packer_image.ubuntu_us_east_2.cloud_image_id
+    condition     = data.aws_instance.app_server1.ami == data.hcp_packer_image.ubuntu_us_east_2.cloud_image_id
     error_message = "Must use the latest available AMI, ${data.hcp_packer_image.ubuntu_us_east_2.cloud_image_id}."
   }
 }
